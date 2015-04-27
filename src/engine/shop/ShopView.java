fe30e255137994218cd1b566f8eceeab1f561b91
Thomas Puglisi <tom.puglisi@gmail.com>
2015-04-27 19:55:14 -0400

    Trying bug fix

--- src/engine/shop/ShopView.java
+++ src/engine/shop/ShopView.java
@@ -184,7 +184,10 @@ public class ShopView extends Parent {
         Button sellButton = new Button(String.format("Sell for: %d",
                                                      Math.round(gameObject.getValue())));
         base.getChildren().add(sellButton);
-        sellButton.setOnAction(event -> model.sellGameObject(gameObject));
+        sellButton.setOnAction(event -> {
+            model.sellGameObject(gameObject);
+            //removeFromInfoBox();
+        });
         addToInfoBox(base);
     }
 
